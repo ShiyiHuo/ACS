d147dfe2272b5b206ecb1ae191f0aa94a36e1287
Shiyi Huo <shiyihuo@u-wolvix.csc.uvic.ca>
2017-10-24 21:57:13 -0700

    input reading: filter out illegal records

--- acs.c
+++ acs.c
@@ -19,6 +19,9 @@ typedef struct customer_info{ /// use this struct to record the customer informa
   int user_id;
 	int service_time;
 	int arrival_time;
+  double waiting_time_start;
+  double waiting_time_end;
+  double waiting_time_total;
 } customer_info;
 
 /* global variables */
@@ -28,12 +31,13 @@ typedef struct customer_info{ /// use this struct to record the customer informa
 #define MAX_INPUT_SIZE 1024
 #define NQUEUE 4
 #define NCLERK 2
+#define TO_UTIME 1000000
 
 customer_info customer_list[MAX_CUSTOMER];  //list of customers
 int clerk_info[2] = {0,1};  //list of clerks (clerkid)
 
 struct timeval init_time; // record the simulation start time; No need to use mutex_lock when reading this variable since the value would not be changed by thread once the initial time was set.
-double overall_waiting_time = 0.0; // used to add up the overall waiting time for all customers, every customer add their own waiting time to this variable, mutex_lock is necessary.
+double overall_waiting_time; // used to add up the overall waiting time for all customers, every customer add their own waiting time to this variable, mutex_lock is necessary.
 int queue_length[NQUEUE];// real-time queue length information; mutex_lock needed
 
 int C = -1; // to check which clerk awakes customer
@@ -51,12 +55,18 @@ pthread_cond_t convar_clerk[NCLERK];
 
 
 // returns time difference (s) between cur_time and start_time
-float getTimeDifference() {
-	struct timeval nowTime;
-	gettimeofday(&nowTime, NULL);
-	long nowMicroseconds = (nowTime.tv_sec * 100000) + nowTime.tv_usec;
-	long startMicroseconds = (init_time.tv_sec * 100000) + init_time.tv_usec;
-	return (float)(nowMicroseconds - startMicroseconds) / 100000;
+double getTimeDifference() {
+  struct timeval cur_time;
+	double cur_secs, init_secs;
+
+	//pthread_mutex_lock(&start_time_mtex); you may need a lock here
+	init_secs = (init_time.tv_sec + (double) init_time.tv_usec / TO_UTIME);
+	//pthread_mutex_unlock(&start_time_mtex);
+
+	gettimeofday(&cur_time, NULL);
+	cur_secs = (cur_time.tv_sec + (double) cur_time.tv_usec / TO_UTIME);
+
+	return cur_secs - init_secs;
 }
 
 
@@ -65,7 +75,7 @@ void * customer_entry(void * cus_info){
 	// a new customer comes in
 	struct customer_info * p_myInfo = (struct customer_info *) cus_info;
 
-	usleep((p_myInfo->arrival_time)*100000); //sleep as long as arrival time
+	usleep((p_myInfo->arrival_time)*TO_UTIME/10); //sleep as long as arrival time
 
 	fprintf(stdout, "A customer arrives: customer ID %2d. \n", p_myInfo->user_id);
 
@@ -73,7 +83,7 @@ void * customer_entry(void * cus_info){
   int i;
   int min_queue_length = MAX_CUSTOMER+1;
   int shortest_queue;
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < NQUEUE; i++) {
     if (queue_length[i] < min_queue_length) {
       min_queue_length = queue_length[i];
       shortest_queue = i;
@@ -81,15 +91,19 @@ void * customer_entry(void * cus_info){
   }
 
 	pthread_mutex_lock(&mutex_queue[shortest_queue]);
-  queue_length[shortest_queue]++;
 	fprintf(stdout, "A customer enters a queue: the queue ID %1d, and length of the queue %2d. \n", shortest_queue, queue_length[shortest_queue]);
-	/* insert customer into queue */
-
-  int clerk;
+  /* insert customer into queue */
+  queue_length[shortest_queue]++;
+  p_myInfo->waiting_time_start = getTimeDifference();
 	// customers are waiting for a clerk
 	pthread_cond_wait(&convar_queue[shortest_queue], &mutex_queue[shortest_queue]);
 	//Now pthread_cond_wait returned, customer was awaked by one of the clerks
+  p_myInfo->waiting_time_end = getTimeDifference();
+  p_myInfo->waiting_time_total = p_myInfo->waiting_time_end - p_myInfo->waiting_time_start;
+	pthread_mutex_unlock(&mutex_queue[shortest_queue]); //unlock mutex_lock such that other customers can enter into the queue
+
   /* Try to figure out which clerk awaked me, because you need to print the clerk Id information */
+  int clerk;
   if (C == 0) { //clerk 0 awakes me
     clerk = 0;
     C = -1;
@@ -98,12 +112,10 @@ void * customer_entry(void * cus_info){
     C = -1;
   }
 
-	pthread_mutex_unlock(&mutex_queue[shortest_queue]); //unlock mutex_lock such that other customers can enter into the queue
-
 	/* get the current machine time; updates the overall_waiting_time*/
 	fprintf(stdout, "A clerk starts serving a customer: start time %.2f, the customer ID %2d, the clerk ID %1d. \n", getTimeDifference(), p_myInfo->user_id, clerk);
 
-	usleep((p_myInfo->service_time)*1000000);
+	usleep((p_myInfo->service_time)*TO_UTIME/10);
 
 	/* get the current machine time; */
 	fprintf(stdout, "A clerk finishes serving a customer: end time %.2f, the customer ID %2d, the clerk ID %1d. \n", getTimeDifference(), p_myInfo->user_id, clerk);
@@ -127,7 +139,7 @@ void *clerk_entry(void * clerkNum){
     int i;
     int max_queue_length = -1;
     int longest_queue;
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < NQUEUE; i++) {
       if (queue_length[i] > max_queue_length) {
         max_queue_length = queue_length[i];
         longest_queue = i;
@@ -136,7 +148,6 @@ void *clerk_entry(void * clerkNum){
 
     if (max_queue_length == 0) {
       usleep(10);
-      continue;
     } else {  // max_queue_length > 0
   		pthread_mutex_lock(&mutex_queue[longest_queue]);
   		/* updates the queue_length, mutex_lock needed */
@@ -152,7 +163,7 @@ void *clerk_entry(void * clerkNum){
     		pthread_cond_signal(&convar_queue[longest_queue]);
       } else {
         usleep(10);
-        continue;
+        // TODO: check C again
       }
 
   		// unlock mutex so that other customers can get in
@@ -179,43 +190,48 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 
-  int i;
-	/** Read customer information from txt file and store them in the structure you created
-		1. Allocate memory(array, link list etc.) to store the customer information.
-		2. File operation: fopen fread getline/gets/fread ..., store information in data structure you created
-	*/
-  // TODO: need modification!
-  int NCustomers = 0;
+  // read in customer info, filter out records with negative values
+  int NCustomers;
+  int curr_customer = 0;
   FILE *fp = fopen(argv[1],"r");// read fild
 	if(fp==NULL){
-		printf("failed in reading file\n");
+		printf("Error: Failed in reading file\n");
 		return -1;
 	}
-	if(!(fscanf(fp,"%d\n",&NCustomers))){
-		printf("check your input file, the first line is the number of flows");
-		return -1;
-	}
-
+	fscanf(fp,"%d\n",&NCustomers);
 	int id,arrival,service;
-	for(i = 0; i < NCustomers; i++) {
-    // TODO: sanity check - data type and negative numbers
-    if(!(fscanf(fp,"%d:%d,%d\n",&id,&arrival,&service))){
-	     printf("Error: input not in format of 'id:arrival time,service time'");
-	  }
-    // if (id<0 || arrival<0 || service<0) {
-    //
-    // }
-    customer_list[i].user_id=id;
-	  customer_list[i].arrival_time=arrival;
-	  customer_list[i].service_time=service;
+
+  while (curr_customer<NCustomers) {
+  	if((fscanf(fp,"%d:%d,%d\n",&id,&arrival,&service))) {
+      // printf("%d:%d,%d\n",id,arrival,service);
+      //if(!(fscanf(fp,"%d:%d,%d\n",&id,&arrival,&service))){
+  	  //   printf("Error: input not in format of 'id:arrival time,service time'\n");
+      //   exit(1);
+  	  // }
+      if (id<0 || arrival<0 || service<0) {
+        printf("Error: negative input is filtered out\n");
+        NCustomers--;
+        //printf("%d:%d,%d\n",id,arrival,service);
+      } else {
+        customer_list[curr_customer].user_id=id;
+    	  customer_list[curr_customer].arrival_time=arrival;
+    	  customer_list[curr_customer].service_time=service;
+        curr_customer++;
+      }
+    }
   }
-  fclose(fp); // release file descriptor
+  fclose(fp);
 
-  // for(i = 0; i < NCustomers; i++) {
-  //   printf("%d:%d,%d\n", customer_list[i].user_id, customer_list[i].arrival_time, customer_list[i].service_time);
-  // }
+  int i;
+  printf("# of customers: %d\n",NCustomers);
+  for(i = 0; i < NCustomers; i++) {
+    printf("%d:%d,%d\n", customer_list[i].user_id, customer_list[i].arrival_time, customer_list[i].service_time);
+  }
 
-
+  if (NCustomers <= 0){
+		printf("Error: Number of customers should be a positive number\n");
+		return -1;
+	}
 
   //initialize all the condition variable and thread lock will be used
   for (i = 0; i < 4; i++) {
@@ -231,41 +247,38 @@ int main(int argc, char* argv[]) {
   //get global start_time
   gettimeofday(&init_time,NULL);
 
-	//create clerk thread
-	for(i = 0; i < 2; i++){ // number of clerks
-    // clerk_info: passing the clerk information (e.g., clerk ID) to clerk thread
+	//create clerk and customer threads
+	for(i = 0; i < NCLERK; i++){
 		pthread_create(&clerk_threads[i], NULL, clerk_entry, (void *)&clerk_info[i]);
 	}
 
-	//create customer thread
-	for(i = 0; i < NCustomers; i++) { // number of customers
-    //custom_info: passing the customer information (e.g., customer ID, arrival time, service time, etc.) to customer thread
+	for(i = 0; i < NCustomers; i++) {
 		pthread_create(&customer_threads[i], NULL, customer_entry, (void *)&customer_list[i]);
 	}
 
 	// wait for all customer threads to terminate
-	for(i = 0; i < NCustomers; i++) { // number of customers
+	for(i = 0; i < NCustomers; i++) {
 		pthread_join(customer_threads[i], NULL);
 	}
 
-	// destroy mutex & condition variable (optional)
-	for (i = 0; i < 4; i++) {
+	// destroy mutex & condition variable
+	for (i = 0; i < NQUEUE; i++) {
 		pthread_mutex_destroy(&mutex_queue[i]);
 		pthread_cond_destroy(&convar_queue[i]);
 	}
 
-	for (i = 0; i < 2; i++) {
+	for (i = 0; i < NCLERK; i++) {
 		pthread_mutex_destroy(&mutex_clerk[i]);
 		pthread_cond_destroy(&convar_clerk[i]);
 	}
 
 	//calculate the average waiting time of all customers
-  // overall_waiting_time = getTimeDifference();
-  // double avg_waiting_time = overall_waiting_time/NCustomers;
-  // fprintf(stdout, "The average waiting time for all customers in the system is %.2f seconds. \n", avg_waiting_time);
-
-
-
+  overall_waiting_time = 0.0;
+  for(i = 0; i < NCustomers; i++) { // number of customers
+	   overall_waiting_time += customer_list[i].waiting_time_total;
+	}
+  double avg_waiting_time = overall_waiting_time/NCustomers;
+  fprintf(stdout, "The average waiting time for all customers in the system is %.2f seconds. \n", avg_waiting_time);
 
 	return 0;
 }
